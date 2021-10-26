5fb7af35fef1a1b2cfb50a75272312be666aadf3
Dyl-Richardson <dylanrichardson1606@gmail.com>
2021-10-26 15:58:59 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,53 @@
+const go = document.querySelector(".go")
+
+function createEvent() {
+    let div = document.createElement("div")
+    div.className = "event"
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    button.className = "remove"
+    button.addEventListener("click", e => {
+        button.parentElement.remove()
+    })
+    button.innerText = "x"
+
+    const eventList = document.querySelector(".eventList")
+    const text = document.querySelector(".text").value
+    const date = document.querySelector(".date").value
+
+    setInterval(function(){
+        let todayDate = new Date()
+        let targetDate = new Date(date)
+        let result = targetDate - todayDate
+
+        let weeks =  Math.floor(result/(1000 * 60 * 60 *24 * 7));
+        let days = Math.floor(result / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((result % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((result % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((result % (1000 * 60)) / 1000);
+
+        if (result >= 604800000) {
+            p.innerText = weeks + "w " + " until " + text;
+        }
+        else if (result >= 86400000) {
+            p.innerText = days + "d " + hours + "h " + " until " + text; 
+        }
+        else if (result >= 3600000) {
+            p.innerText = hours + "h " + minutes + "m " + seconds + "s " + " until " + text;
+        }
+        else if (result <= 0) {
+            p.innerText = ""
+        }
+        
+    },1000)
+   p.appendChild(button)
+   
+    eventList.appendChild(div)
+    div.appendChild(button)
+    div.appendChild(p)
+    
+}
+
+go.addEventListener("click", e => {
+    createEvent() 
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Exercice
+https://github.com/becodeorg/BXL-Hopper-2-33/tree/master/2.The-Hill/Projects/3.The-Final-Countdown
